4a0e7b83103e37a36fd03d11f2bd9d7a75197c74
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-08-20 16:21:09 +0300

    Create and bundle drop-down menu package.

---
+++ dist/98cb3919ebf6a77bf455.js
@@ -0,0 +1,16 @@
+import "./style.css"
+
+function hamburgerClicked() {
+  const hamburger = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", dropDown);
+}
+
+// Drop down items
+function dropDown() {
+  console.log("event clicked");
+  const hamburgerItems = document.querySelector(".menu-items");
+  hamburgerItems.classList.toggle("hidden");
+}
+
+hamburgerClicked();
